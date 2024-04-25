a033bceeb350514140222eb6911112359a1f0b71
ariba1039 <aribahussain970@gmail.com>
2024-04-25 14:40:38 +0500

    listview_animations

---
+++ lib/screens/listview_animations.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_animation/models/animatedlist.model.dart';
+
+class ListViewAnimations extends StatefulWidget {
+  const ListViewAnimations({super.key});
+
+  @override
+  State<ListViewAnimations> createState() => _ListViewAnimationsState();
+}
+
+class _ListViewAnimationsState extends State<ListViewAnimations> {
+  double width = 0;
+  bool myAnimation = false;
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      setState(() {
+        myAnimation = true;
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 34, 36, 49),
+      body: SafeArea(
+        child: ListView(
+          physics: const BouncingScrollPhysics(),
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          children: [
+            const Center(
+              child: Text(
+                'ListView Animations ',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  fontSize: 25,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            ListView.builder(
+                physics: const BouncingScrollPhysics(),
+                shrinkWrap: true,
+                itemCount: users.length,
+                itemBuilder: (context, index) {
+                  final myUser = users[index];
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: AnimatedContainer(
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(
+                            20,
+                          )),
+                      duration: Duration(milliseconds: 400 + (index * 400)),
+                      curve: Curves.easeIn,
+                      transform: Matrix4.translationValues(
+                          myAnimation ? 0 : width, 0, 0),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          children: [
+                            CircleAvatar(
+                              radius: 35,
+                              backgroundImage: NetworkImage(myUser.image),
+                            ),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  myUser.name,
+                                  style: const TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                                Text(
+                                  myUser.username,
+                                  style: const TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                        color: Colors.grey,    
+                                    
+                                    
+              
+                                  ),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                }),
+            const SizedBox(
+              height: 50,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
